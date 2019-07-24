2133c483482d8053b60440e248397ae17952bf58
Nathan Gideon <nathan@idevworks.com>
2019-07-24 19:18:16 +0100

    Add project files.

---
+++ CabinetDimension/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace CabinetDimension
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabTopCabinet = new System.Windows.Forms.TabControl();
+            this.tbpTopCabinet = new System.Windows.Forms.TabPage();
+            this.tbpBottomCabinet = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtPlywoodThickness = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOverallHeight = new System.Windows.Forms.TextBox();
+            this.txtToeKickHeight = new System.Windows.Forms.TextBox();
+            this.txtToeKickDepth = new System.Windows.Forms.TextBox();
+            this.txtCabinetDepth = new System.Windows.Forms.TextBox();
+            this.txtCabinetWidth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nupdQuantity = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCalculateBottomDimension = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clumQuantity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabTopCabinet.SuspendLayout();
+            this.tbpBottomCabinet.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupdQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabTopCabinet
+            // 
+            this.tabTopCabinet.Controls.Add(this.tbpTopCabinet);
+            this.tabTopCabinet.Controls.Add(this.tbpBottomCabinet);
+            this.tabTopCabinet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabTopCabinet.Location = new System.Drawing.Point(0, 0);
+            this.tabTopCabinet.Name = "tabTopCabinet";
+            this.tabTopCabinet.SelectedIndex = 0;
+            this.tabTopCabinet.Size = new System.Drawing.Size(611, 507);
+            this.tabTopCabinet.TabIndex = 0;
+            // 
+            // tbpTopCabinet
+            // 
+            this.tbpTopCabinet.Location = new System.Drawing.Point(4, 22);
+            this.tbpTopCabinet.Name = "tbpTopCabinet";
+            this.tbpTopCabinet.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpTopCabinet.Size = new System.Drawing.Size(515, 406);
+            this.tbpTopCabinet.TabIndex = 0;
+            this.tbpTopCabinet.Text = "Top Cabinet";
+            this.tbpTopCabinet.UseVisualStyleBackColor = true;
+            // 
+            // tbpBottomCabinet
+            // 
+            this.tbpBottomCabinet.Controls.Add(this.listView1);
+            this.tbpBottomCabinet.Controls.Add(this.btnCalculateBottomDimension);
+            this.tbpBottomCabinet.Controls.Add(this.tableLayoutPanel1);
+            this.tbpBottomCabinet.Location = new System.Drawing.Point(4, 22);
+            this.tbpBottomCabinet.Name = "tbpBottomCabinet";
+            this.tbpBottomCabinet.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpBottomCabinet.Size = new System.Drawing.Size(603, 481);
+            this.tbpBottomCabinet.TabIndex = 1;
+            this.tbpBottomCabinet.Text = "Bottom Cabinet";
+            this.tbpBottomCabinet.UseVisualStyleBackColor = true;
+            this.tbpBottomCabinet.Click += new System.EventHandler(this.tbpBottomCabinet_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.77551F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.22449F));
+            this.tableLayoutPanel1.Controls.Add(this.txtPlywoodThickness, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtOverallHeight, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtToeKickHeight, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtToeKickDepth, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtCabinetDepth, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtCabinetWidth, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.nupdQuantity, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 116F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 201);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // txtPlywoodThickness
+            // 
+            this.txtPlywoodThickness.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPlywoodThickness.Location = new System.Drawing.Point(113, 135);
+            this.txtPlywoodThickness.Name = "txtPlywoodThickness";
+            this.txtPlywoodThickness.Size = new System.Drawing.Size(168, 20);
+            this.txtPlywoodThickness.TabIndex = 14;
+            this.txtPlywoodThickness.Text = "1.5";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 164);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Quantity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Cabinet Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Cabinet Depth";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Toe Kick Depth";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Toe Kick Height";
+            // 
+            // txtOverallHeight
+            // 
+            this.txtOverallHeight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOverallHeight.Location = new System.Drawing.Point(113, 3);
+            this.txtOverallHeight.Name = "txtOverallHeight";
+            this.txtOverallHeight.Size = new System.Drawing.Size(168, 20);
+            this.txtOverallHeight.TabIndex = 1;
+            this.txtOverallHeight.Text = "90";
+            // 
+            // txtToeKickHeight
+            // 
+            this.txtToeKickHeight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtToeKickHeight.Location = new System.Drawing.Point(113, 28);
+            this.txtToeKickHeight.Name = "txtToeKickHeight";
+            this.txtToeKickHeight.Size = new System.Drawing.Size(168, 20);
+            this.txtToeKickHeight.TabIndex = 2;
+            this.txtToeKickHeight.Text = "10";
+            // 
+            // txtToeKickDepth
+            // 
+            this.txtToeKickDepth.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtToeKickDepth.Location = new System.Drawing.Point(113, 49);
+            this.txtToeKickDepth.Name = "txtToeKickDepth";
+            this.txtToeKickDepth.Size = new System.Drawing.Size(168, 20);
+            this.txtToeKickDepth.TabIndex = 3;
+            this.txtToeKickDepth.Text = "8";
+            // 
+            // txtCabinetDepth
+            // 
+            this.txtCabinetDepth.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtCabinetDepth.Location = new System.Drawing.Point(113, 79);
+            this.txtCabinetDepth.Name = "txtCabinetDepth";
+            this.txtCabinetDepth.Size = new System.Drawing.Size(168, 20);
+            this.txtCabinetDepth.TabIndex = 4;
+            this.txtCabinetDepth.Text = "56";
+            // 
+            // txtCabinetWidth
+            // 
+            this.txtCabinetWidth.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtCabinetWidth.Location = new System.Drawing.Point(113, 108);
+            this.txtCabinetWidth.Name = "txtCabinetWidth";
+            this.txtCabinetWidth.Size = new System.Drawing.Size(168, 20);
+            this.txtCabinetWidth.TabIndex = 5;
+            this.txtCabinetWidth.Text = "60";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Overall Height";
+            // 
+            // nupdQuantity
+            // 
+            this.nupdQuantity.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nupdQuantity.Location = new System.Drawing.Point(113, 167);
+            this.nupdQuantity.Name = "nupdQuantity";
+            this.nupdQuantity.Size = new System.Drawing.Size(168, 20);
+            this.nupdQuantity.TabIndex = 6;
+            this.nupdQuantity.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Plywood Thickness";
+            // 
+            // btnCalculateBottomDimension
+            // 
+            this.btnCalculateBottomDimension.Location = new System.Drawing.Point(408, 183);
+            this.btnCalculateBottomDimension.Name = "btnCalculateBottomDimension";
+            this.btnCalculateBottomDimension.Size = new System.Drawing.Size(111, 34);
+            this.btnCalculateBottomDimension.TabIndex = 1;
+            this.btnCalculateBottomDimension.Text = "Calculate";
+            this.btnCalculateBottomDimension.UseVisualStyleBackColor = true;
+            this.btnCalculateBottomDimension.Click += new System.EventHandler(this.btnCalculateBottomDimension_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader3,
+            this.columnHeader2,
+            this.clumQuantity});
+            this.listView1.Location = new System.Drawing.Point(9, 237);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(340, 156);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 80;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Width(cm)";
+            this.columnHeader2.Width = 80;
+            // 
+            // clumQuantity
+            // 
+            this.clumQuantity.Text = "Quantity";
+            this.clumQuantity.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Length(cm)";
+            this.columnHeader3.Width = 80;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 507);
+            this.Controls.Add(this.tabTopCabinet);
+            this.Name = "Form1";
+            this.Text = "Cabinet Dimension Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabTopCabinet.ResumeLayout(false);
+            this.tbpBottomCabinet.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupdQuantity)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabTopCabinet;
+        private System.Windows.Forms.TabPage tbpTopCabinet;
+        private System.Windows.Forms.TabPage tbpBottomCabinet;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtPlywoodThickness;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOverallHeight;
+        private System.Windows.Forms.TextBox txtToeKickHeight;
+        private System.Windows.Forms.TextBox txtToeKickDepth;
+        private System.Windows.Forms.TextBox txtCabinetDepth;
+        private System.Windows.Forms.TextBox txtCabinetWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nupdQuantity;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCalculateBottomDimension;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader clumQuantity;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ CabinetDimension/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CabinetDimension
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnCalculateBottomDimension_Click(object sender, EventArgs e)
+        {   
+
+            var H = float.Parse(txtOverallHeight.Text);
+            var TH = float.Parse(txtToeKickHeight.Text);
+            var TD = float.Parse(txtToeKickDepth.Text);
+            var D = float.Parse(txtCabinetDepth.Text);
+            var W = float.Parse(txtCabinetWidth.Text);
+            var T = float.Parse(txtPlywoodThickness.Text);
+            var N = W - (2 * T);
+            var Q = int.Parse(nupdQuantity.Value.ToString());
+
+            var sides = new ListViewItem(new string[] { "sides", D.ToString(), H.ToString(), (2 * Q).ToString() });
+            var floors = new ListViewItem(new string[] { "floors", D.ToString(), N.ToString(), (2 * Q).ToString() });
+            var strips = new ListViewItem(new string[] { "strips", TH.ToString(), N.ToString(), (4 * Q).ToString() });
+            var doors = new ListViewItem(new string[] { "doors",(W/2).ToString(), (H - TH).ToString(), (2 * Q).ToString() });
+
+            listView1.Items.Clear();
+            listView1.Items.AddRange(new ListViewItem[] { sides, floors, strips, doors });
+        }
+
+        private void tbpBottomCabinet_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
